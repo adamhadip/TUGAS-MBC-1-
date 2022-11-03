4747d994d8ac4d2f485018bfa8f90020778729a3
maverick211001 <dsyogas07@gmail.com>
2022-11-03 22:10:36 +0700

    Tambah Searching

--- contactphone.py
+++ contactphone.py
@@ -17,3 +17,10 @@ for i in range(numbers):
 search_term = input("\nEnter search term: ")
 
 print("Search result:")
+if search_term in name:
+    index = name.index(search_term)
+    phone_number = nohp.[index]
+    print("Name: {}, Phone Number: {}".format(search_term, nohp))
+
+else:
+    print("Name Not Found")
