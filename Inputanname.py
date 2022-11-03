888c1edd32a640c4692b542c0e630622235628e7
adamhadip <adamhadip@student.telkomuniversity.ac.id>
2022-11-03 21:34:24 +0700

    BUAT INPUTAN NAMA DAN NO HP

---
+++ Inputanname.py
@@ -0,0 +1,12 @@
+nohp = []
+name = [] 
+numbers = 3 
+
+for k in range(numbers) : 
+    yourname = input("Nama : ")
+    phone_numb = int(input("No Phone : "))
+
+    name.append(yourname)
+    nohp.append(phone_numb)
+
+print ("\n Name \t\t\t No Phone \n")
